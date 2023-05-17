62c2f151d00d48fdbde6c970a3a5190966c7bce0
Conner McCarthy <conner.mccarthy.97@gmail.com>
2023-05-17 18:37:37 -0400

    adds chessle guess board skeleton

---
+++ chessAI-project/chessle/static/js/setupboard.js
@@ -0,0 +1,23 @@
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+
+function initBoard() {
+    let board = document.getElementById("guess-board");
+
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+        
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard()
--- chessAI-project/chessle/tests.py
+++ chessAI-project/chessle/tests.py
@@ -2,12 +2,18 @@ from django.test import TestCase
 from django.urls import reverse
 
 class HomeTest(TestCase):
+    
     def test_home_loads(self):
         url = reverse('home')
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'empty body')
+        self.assertContains(response, 'Chess AI: Chessle')
+
+    def test_chess_board_loads(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertContains(response, 'board1')
 
 # Create your tests here.
 
